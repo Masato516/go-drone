b9139411c759a818093b3778df33f04a40937b12
Masato Yagi <yaginen9@gmail.com>
2022-03-17 15:49:05 +0900

    feat: Webサーバーの追加

--- go_tello_edu/main.go
+++ go_tello_edu/main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"log"
+	"udemy_drone/go_tello_edu/app/controllers"
 	"udemy_drone/go_tello_edu/app/utils"
 	"udemy_drone/go_tello_edu/config"
 )
 
 func main() {
-	utils.LoggingSettings(config.Config.LogFile)
-	log.Println("test")
+	// utils.LoggingSettings(config.Config.LogFile)
 	// droneManager := models.NewDroneManager()
+	// droneManager.Land()
 	// droneManager.TakeOff()
 	// time.Sleep(10 * time.Second)
+	// droneManager.Patrol()
+	// log.Println("パトロール１回目終了")
+	// time.Sleep(30 * time.Second)
+	// droneManager.Patrol()
+	// time.Sleep(10 * time.Second)
 	// droneManager.Land()
+
+	utils.LoggingSettings(config.Config.LogFile)
+	log.Println(controllers.StartWebServer())
 }
