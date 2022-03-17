d9c91a46dd354e275352459e3cd2c2e0220bf3a6
Masato Yagi <yaginen9@gmail.com>
2022-03-17 15:49:46 +0900

    feat: 設定ファイルの作成・設定

--- go_tello_edu/config/config.go
+++ go_tello_edu/config/config.go
@@ -7,44 +7,21 @@ import (
 	"gopkg.in/ini.v1"
 )
 
-// type ConfList struct {
-// 	LogFile string
-// 	Address string
-// 	Port    string
-// }
-//
-// var Config ConfList
-//
-// func init() {
-// 	cfg, err := ini.Load("config.ini")
-// 	if err != nil {
-// 		log.Printf("Failde to read file: %w", err)
-// 		os.Exit(1)
-// 	}
-//
-// 	Config = ConfList{
-// 		LogFile: cfg.Section("go tello").Key("log_file").String(),
-// 		Address: cfg.Section("web").Key("address").String(),
-// 		Port:    cfg.Section("web").Key("port").String(),
-// 	}
-// }
-//
-
-type ConfigList struct {
+type ConfList struct {
 	LogFile string
 	Address string
 	Port    int
 }
 
-var Config ConfigList
+var Config ConfList
 
-func init() {
+func init() { // パッケージがimportされたタイミングで実行
 	cfg, err := ini.Load("config.ini")
 	if err != nil {
 		log.Printf("Failed to read: %v", err)
 		os.Exit(1)
 	}
-	Config = ConfigList{
+	Config = ConfList{
 		LogFile: cfg.Section("go_tello_edu").Key("log_file").String(),
 		Address: cfg.Section("web").Key("address").String(),
 		Port:    cfg.Section("web").Key("port").MustInt(),
